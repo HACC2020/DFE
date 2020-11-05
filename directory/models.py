d33be3e1fb844d7f5d714681d1ea2436118e2180
Katie Kimura <2022kimurak@milianihs.k12.hi.us>
2020-11-04 19:02:02 -1000

    created models for tables

--- directory/models.py
+++ directory/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class applications(models.Model):
+    name = models.CharField(max_length=100)
+    ownerAgencyName = models.CharField(max_length=100)
+
+class projects(models.Model):
+    ownerAgencyName = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+class usergroups(models.Model):
+    name = models.CharField(max_length=100)
+
+# class averages(models.Model):
