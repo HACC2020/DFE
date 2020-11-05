50a0df562bc65f04012c71cab1ff99e49761a442
Katie Kimura <2022kimurak@milianihs.k12.hi.us>
2020-11-05 13:16:04 -1000

    about page integrated in

--- directory/urls.py
+++ directory/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path ('', views.directory, name='directory')
+    path('', views.about, name='about')
+    #above is just a test
+    #path('', views.directory, name='directory'),
+    #path('about/', views.about, name='about')
 ]
--- directory/views.py
+++ directory/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-def work(request, folder_id):
+#def work(request, folder_id):
 
-# does organization exist within database
+#does organization exist within database
+
+def about(request):
+    return render(request, 'about.html')
