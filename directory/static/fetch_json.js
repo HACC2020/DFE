fc419eabe26c4eedded9757def8fb57fa03d70af
Shane Parslow <shaneparslow808@gmail.com>
2020-11-05 15:55:34 -1000

    add jquery, work on fetching function

--- directory/static/fetch_json.js
+++ directory/static/fetch_json.js
@@ -1,15 +1,15 @@
 function get_json () {
 	var data = {
 		resource_id: '9468a555-8d1f-42fb-b1a9-c3ae0d5c756d', // the resource id
-		limit: 5, // get 5 results
-		q: 'jones' // query for 'jones'
+		limit: 5 // get 5 results
 	};
 	$.ajax({
 		url: 'https://opendata.hawaii.gov/api/3/action/datastore_search',
 		data: data,
-		dataType: 'jsonp',
+		dataType: 'json',
 		success: function(data) {
-			alert('Total results found: ' + data.result.total)
+			alert('Total results found: ' + data.result.total);
+			console.log(data.
 		}
 	});
 }
