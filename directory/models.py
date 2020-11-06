da933fb56e6e048fa3f9d625013eaaa3ee120110
Katie Kimura <2022kimurak@milianihs.k12.hi.us>
2020-11-05 15:42:53 -1000

    working on models integration

--- directory/models.py
+++ directory/models.py
@@ -5,11 +5,27 @@ class applications(models.Model):
     name = models.CharField(max_length=100)
     ownerAgencyName = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class projects(models.Model):
     ownerAgencyName = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    #when call upon this in admin, displays name within admin,
+    #helps populate table in admin
+    #ex. return self.breed (return data in breed colummn)
+
 class usergroups(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+'''
+    class Meta:
+        ordering = ['name']
+'''
 # class averages(models.Model):
--- directory/views.py
+++ directory/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import usergroups
 # Create your views here.
 #def work(request, folder_id):
 
