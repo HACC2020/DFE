9b381a08300a634fd76be17c273ca7a37a2bf277
Katie Kimura <2022kimurak@milianihs.k12.hi.us>
2020-11-06 16:47:11 -1000

    added buttons on bottom of departments page for nav

--- directory/views.py
+++ directory/views.py
@@ -21,16 +21,26 @@ def contact(request):
 
 def departments(request, page):
     usergroups_var = usergroups.objects.all().order_by('parent')
+    pages = []
     subdepartments = []
     page_var = len(usergroups_var)/25
     page_var = round(page_var)
     t = 0
     p = 25
+
     if int(page) != 1:
         for i in range(0,int(page)):
             t += 25
             p += 25
+
     for i in range(t,p):
-        subdepartments.append(usergroups_var[i])
-    return render(request, 'departments.html', {'name': subdepartments})
+        try:
+            subdepartments.append(usergroups_var[i])
+        except IndexError:
+            pass
+
+    for i in range(1,int(page_var)+1):
+        pages.append(i)
+
+    return render(request, 'departments.html', {'name': subdepartments, 'page': pages})
 
