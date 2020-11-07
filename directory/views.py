c18e113b779ee04e1ecba14b080d7cd2782ee445
Katie Kimura <2022kimurak@milianihs.k12.hi.us>
2020-11-06 15:05:20 -1000

    added page function;only 25 depts listed per page

--- directory/views.py
+++ directory/views.py
@@ -13,11 +13,27 @@ from .models import percents
 def home(request):
     return render(request, 'directory.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def contact(request):
     return render(request, 'contact.html')
 
-def departments(request):
-    return render(request, 'departments.html')
+
+def departments(request, page):
+    usergroups_var = usergroups.objects.all().order_by('parent')
+    subdepartments = []
+    page_var = len(usergroups_var)/25
+    page_var = round(page_var)
+    t = 0
+    p = 25
+    if int(page) != 1:
+        for i in range(0,int(page)):
+            t += 25
+            p += 25
+    for i in range(t,p):
+        subdepartments.append(usergroups_var[i])
+    return render(request, 'departments.html', {'name': subdepartments})
+
