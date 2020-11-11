f7df1fabe196ffc752805bdb487e2c5cf650cb14
Katie Kimura <2022kimurak@milianihs.k12.hi.us>
2020-11-10 20:45:22 -1000

    nothing changes

--- directory/views.py
+++ directory/views.py
@@ -104,11 +104,11 @@ def application(request, departments):
 
 
 def projectpage(request, project):
-     projects_var = projects.objects.filter(name=project)
-     for i in projects_var:
+    projects_var = projects.objects.filter(name=project)
+    for i in projects_var:
         listofapps = i.applications.split(';')
-     return render(request, 'project.html', {'projectinfo': projects_var, 'listofapps': listofapps})
-
+    #print(projects_var)
+    return render(request, 'project.html', {'projectinfo': projects_var, 'listofapps': listofapps})
 
 
 
